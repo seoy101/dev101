7ac5fec65ba41740d3b0be15e47f2472a2e9afbb
Kam <mjkam92@hanmail.net>
2016-07-27 11:20:06 +0900

    nfs dockerfile 수정

--- Kam/run.sh
+++ Kam/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-echo "/mnt *(rw,insecure,fsid=0,no_subtree_check)" >> /etc/exports
+echo "/nfsdir *(rw,insecure,fsid=0,no_subtree_check)" >> /etc/exports
 rm /etc/services
-mv /minjae/conf/services /etc/
+mv /nfsconf/services /etc/
 rm /etc/default/nfs-kernel-server
-mv /minjae/conf/nfs-kernel-server /etc/default/nfs-kernel-server
+mv /nfsconf/nfs-kernel-server /etc/default/nfs-kernel-server
 
 service rpcbind start
 service nfs-kernel-server start
